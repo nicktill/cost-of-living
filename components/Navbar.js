aaefb8d75e33bcd4ac498f1c3bde1624305bd332
Nicholas Tillmann <ntillmann1439@gmail.com>
2022-12-16 03:06:19 -0500

    icons for navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaCalculator, FaUser, FaMoon } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [isLightMode, setIsLightMode] = useState(true);
+
+  const handleLightModeClick = () => {
+    setIsLightMode(!isLightMode);
+  };
+
   return (
     <div className="flex items-center justify-between px-4 py-3">
-      <h1 className="text-2xl font-bold text-gray-300">COL</h1>
-      <button className="px-4 py-2 rounded-full text-gray-300 hover:text-white">
-        <svg className="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <path d="M12 2c2.21 0 4 1.79 4 4s-1.79 4-4 4-4-1.79-4-4 1.79-4 4-4zm0 10c4.42 0 8 1.79 8 4v2h-16v-2c0-2.21 3.58-4 8-4z"/>
-        </svg>
-      </button>
+      <FaCalculator className="text-2xl font-bold text-gray-300" />
+      <div className="flex-1 text-right">
+        <button
+          className="px-4 py-2 rounded-full text-gray-300 hover:text-white"
+          onClick={handleLightModeClick}
+        >
+          <FaMoon className={`h-6 w-6 fill-current ${isLightMode ? 'text-gray-300' : 'text-slate-100'}`} />
+        </button>
+        <button className="px-4 py-2 rounded-full text-gray-300 hover:text-white">
+          <FaUser className="h-6 w-6 fill-current" />
+        </button>
+      </div>
     </div>
   );
 };
